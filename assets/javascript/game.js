ca45ab39ed777dd78232677f3c812b3142bee5a3
Kevin Abram <htwnkid@gmail.com>
2019-07-22 22:44:39 -0400

    Version 2 of game. Most functionality is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
 
+    let totalWins = 0;
+    let totalLosses = 0;
+    let currentMatchScore = 0;
+
+
     function genRandNbr (minnbr,maxnbr) {
 
         // Kevin verify that these fuctions are using the correct parameters
@@ -13,7 +18,7 @@ $(document).ready(function () {
 
     }
 
-    function setMaxScore () {
+    function setMatchScore () {
 
         let randNumber = 0;
         let MatchScoreNbr = "";
@@ -27,8 +32,58 @@ $(document).ready(function () {
 
         //console.log("randNumber = " + randNumber);
 
-        MatchScoreNbr.textContent = randNumber;
+        currentMatchScore = randNumber;
 
+        MatchScoreNbr.textContent = randNumber;
+        
+    }
+
+    function resetTotalScore () {
+
+        let lv_totalScore = 0;
+        let lv_totalScore_ptr = "";
+        
+        //Obtain the currently displayed totalLosses value displayed on screen
+
+        lv_totalScore_ptr = document.querySelector('#totalScore');
+
+        lv_totalScore_ptr.textContent = lv_totalScore = 0;
+        
+    }
+
+
+    function setTotalLosses () {
+
+        let lv_totalLosses = 0;
+        let lv_totalloss_ptr = "";
+        
+        //Obtain the currently displayed totalLosses value displayed on screen
+
+        lv_totalloss_ptr = document.querySelector('#totlossScore');
+
+        lv_totalLosses = Number(lv_totalloss_ptr.textContent);
+
+        lv_totalLosses += lv_totalLosses + 1;
+
+        lv_totalloss_ptr.textContent = lv_totalLosses;
+        
+    }
+
+    function setTotalWins () {
+
+        let lv_totalWins = 0;
+        let lv_totalWins_ptr = "";
+        
+        //Obtain the currently displayed totalLosses value displayed on screen
+
+        lv_totalWins_ptr = document.querySelector('#totwinScore');
+
+        lv_totalWins_ptr = Number(lv_totalWins_ptr.textContent);
+
+        lv_totalWins += lv_totalWins + 1;
+
+        lv_totalWins_ptr.textContent = lv_totalWins;
+        
     }
 
     function greenclick () {
@@ -39,13 +94,38 @@ $(document).ready(function () {
 
         let randNumber = genRandNbr (1,12);
     
-        console.log("Current GreenStone Value is: " + randNumber);
-    
-        //newTotalScore = currentTotalScore.textContent + randNumber;
+        //console.log("Current GreenStone Value is: " + randNumber);
+            
+        //console.log("Current TotalScore = " + Number(currentTotalScore.textContent));
 
-        console.log("Current TotalScore = " + Number(currentTotalScore.textContent));
+        newTotalScore = Number(currentTotalScore.textContent) + Number(randNumber);
 
         currentTotalScore.textContent = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        if (newTotalScore > currentMatchScore) {
+
+            setTotalLosses ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Lost the Game");
+            
+
+        } else if (newTotalScore > currentMatchScore) {
+
+            setTotalWins ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Won the Game");
+            
+
+        }
+        
    
     }
 
@@ -54,27 +134,81 @@ $(document).ready(function () {
         let newTotalScore = 0;
     
         let currentTotalScore = document.querySelector('#totalScore');
+
         let randNumber = genRandNbr (1,12);
     
-        console.log("Current GreenStone Value is: " + randNumber);
-    
-        newTotalScore = currentTotalScore.textContent + randNumber;
-    
-    
-    }
+        //console.log("Current GreenStone Value is: " + randNumber);
+            
+        //console.log("Current TotalScore = " + Number(currentTotalScore.textContent));
 
+        newTotalScore = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        currentTotalScore.textContent = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        if (newTotalScore > currentMatchScore) {
+
+            setTotalLosses ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Lost the Game");
+
+        } else if (newTotalScore > currentMatchScore) {
+
+            setTotalWins ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Won the Game");
+
+        }
+        
+   
+    }
+    
     function yellowclick () {
 
         let newTotalScore = 0;
     
         let currentTotalScore = document.querySelector('#totalScore');
+
         let randNumber = genRandNbr (1,12);
     
-        console.log("Current GreenStone Value is: " + randNumber);
-    
-        newTotalScore = currentTotalScore.textContent + randNumber;
-    
-    
+        //console.log("Current GreenStone Value is: " + randNumber);
+            
+        //console.log("Current TotalScore = " + Number(currentTotalScore.textContent));
+
+        newTotalScore = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        currentTotalScore.textContent = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        if (newTotalScore > currentMatchScore) {
+
+            setTotalLosses ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Lost the Game");
+
+        } else if (newTotalScore > currentMatchScore) {
+
+            setTotalWins ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Won the Game");
+
+        }
+        
+   
     }
 
     function purpleclick () {
@@ -82,19 +216,46 @@ $(document).ready(function () {
         let newTotalScore = 0;
     
         let currentTotalScore = document.querySelector('#totalScore');
+
         let randNumber = genRandNbr (1,12);
     
-        console.log("Current GreenStone Value is: " + randNumber);
-    
-        newTotalScore = currentTotalScore.textContent + randNumber;
-    
-    
+        //console.log("Current GreenStone Value is: " + randNumber);
+            
+        //console.log("Current TotalScore = " + Number(currentTotalScore.textContent));
+
+        newTotalScore = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        currentTotalScore.textContent = Number(currentTotalScore.textContent) + Number(randNumber);
+
+        if (newTotalScore > currentMatchScore) {
+
+            setTotalLosses ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Lost the Game");
+
+        } else if (newTotalScore > currentMatchScore) {
+
+            setTotalWins ();
+
+            resetTotalScore ();
+
+            setMatchScore ();
+
+            alert("You Won the Game");
+
+        }
+        
+   
     }
 
     
     //Main Section
     
-    setMaxScore();  //Call the setMaxScore function
+    setMatchScore();  //Call the setMatchScore function
     
     $("#greenStone").on("click", greenclick);
 
